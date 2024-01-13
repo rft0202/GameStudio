99edd914dae397c6f6f644df76933c7f77a0865c
Matthew6596 <114946013+Matthew6596@users.noreply.github.com>
2024-01-13 16:10:03 -0500

    Reticle script readability edit

--- UnityProject/GameStudio/Assets/Scripts/ReticleScript.cs
+++ UnityProject/GameStudio/Assets/Scripts/ReticleScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,10 @@ using UnityEngine.InputSystem;
 
 public class ReticleScript : MonoBehaviour
 {
+    [SerializeField]
+    [Tooltip("Equal to the X and Y Scale of a rectangle sprite that covers the whole screen")]
+    private float GameScreenWidth=17.775f,GameScreenHeight=10;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +24,7 @@ public class ReticleScript : MonoBehaviour
             Debug.Log(mousePixelPos);
             mousePixelPos = new Vector2(mousePixelPos.x-(Screen.width / 2),mousePixelPos.y-(Screen.height / 2)); //(0,0) is center
             //Convert from pixels to Unity's (approx) Units
-            transform.position = new Vector2((mousePixelPos.x / Screen.width) * 17.775f, (mousePixelPos.y / Screen.height) * 10);
+            transform.position = new Vector2((mousePixelPos.x / Screen.width) * GameScreenWidth, (mousePixelPos.y / Screen.height) * GameScreenHeight);
         }
     }
 }
