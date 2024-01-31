ab1324cbe052b89c81eec317fad9900aad8227fe
Matthew6596 <114946013+Matthew6596@users.noreply.github.com>
2024-01-31 13:00:28 -0500

    Started sound manager

---
+++ UnityProject/GameStudio/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    [SerializeField]
+    AudioSource[] srcs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlaySFX(AudioClip clip, float pitch=1)
+    {
+        int srcNum = FirstOpenSrc();
+        srcs[srcNum].pitch = pitch;
+        srcs[srcNum].PlayOneShot(clip);
+    }
+
+    public void EndAllSFX()
+    {
+        for (int i = 0; i < srcs.Length; i++)
+        {
+            srcs[i].Stop();
+        }
+    }
+
+    public void PauseAllSFX(bool pause)
+    {
+        for(int i=0; i<srcs.Length; i++)
+        {
+            if (pause) srcs[i].Pause(); else srcs[i].UnPause();
+        }
+    }
+
+    int FirstOpenSrc()
+    {
+        int ind = 0;
+        foreach (AudioSource _src in srcs)
+        {
+            if (!_src.isPlaying) return ind;
+            ind++;
+        }
+        return ind;
+    }
+}
