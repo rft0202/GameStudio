0be9d240f4bf886a7f17c757f3f24f22b08df06d
rft0202 <rftester@email.neit.edu>
2024-02-11 15:37:50 -0500

    fixed the level select arrows

--- UnityProject/GameStudio/Assets/Scripts/LevelSelectArrows.cs
+++ UnityProject/GameStudio/Assets/Scripts/LevelSelectArrows.cs
@@ -11,14 +11,20 @@ public class LevelSelectArrows : MonoBehaviour
     public Animator level3Anim;
 
     //level vars
-    int currLevel = 1;
+    public int currLevel = 1;
     public bool level2Unlocked = false;
     public bool level3Unlocked = false;
 
     private void Start()
     {
         sm = GameObject.Find("SoundManager").GetComponent<SoundManager>();
-        
+        InvokeRepeating("SetCurrLevel", 1.0f, 1.0f);
+    }
+
+    //Set Curr Level
+    void SetCurrLevel()
+    {
+        currLevel = level2Anim.GetInteger("currLevel");
     }
 
     //LEVEL SELECT ARROWS
@@ -29,29 +35,28 @@ public class LevelSelectArrows : MonoBehaviour
         {
             //Level 2 to right
             level2Anim.SetTrigger("right");
-            //Level 1 to left
-            level1Anim.SetTrigger("left");
+            //Level 1 to right
+            level1Anim.SetTrigger("right");
 
             currLevel = 1;
             level2Anim.SetInteger("currLevel", currLevel);
         }
         else if(currLevel == 3)
         {
+            //Level 2 to right
+            level2Anim.SetTrigger("right");
             //Level 3 to right
             level3Anim.SetTrigger("right");
-            //Level 2 to left
-            level2Anim.SetTrigger("left");
 
             currLevel = 2;
             level2Anim.SetInteger("currLevel", currLevel);
         }
-
     }
 
     public void Right()
     {
         sm.PlaySFX(4);
-        if(currLevel == 1 && level2Unlocked)
+        if (currLevel == 1 && level2Unlocked)
         {
             //Level 1 to Left
             level1Anim.SetTrigger("left");
@@ -64,12 +69,13 @@ public class LevelSelectArrows : MonoBehaviour
         else if(currLevel == 2 && level3Unlocked)
         {
             //Level 2 to left
-            level2Anim.SetTrigger("right");
+            level2Anim.SetTrigger("left");
             //Level 3 to left
             level3Anim.SetTrigger("left");
 
             currLevel = 3;
             level2Anim.SetInteger("currLevel", currLevel);
         }
+        currLevel = level2Anim.GetInteger("currLevel");
     }
 }
