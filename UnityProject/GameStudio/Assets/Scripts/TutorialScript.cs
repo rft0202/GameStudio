f3b01a167e7bf2177f63ac1abda5446b48661802
Matthew6596 <114946013+Matthew6596@users.noreply.github.com>
2024-02-18 14:11:36 -0500

    Updated tutorial buttons slightly

--- UnityProject/GameStudio/Assets/Scripts/TutorialScript.cs
+++ UnityProject/GameStudio/Assets/Scripts/TutorialScript.cs
@@ -12,6 +12,9 @@ public class TutorialScript : MonoBehaviour
     public GameObject[] enemies;
     List<GameObject> activeEnemies = new();
 
+    Color[] btnUnpressedColors = { Color.red, Color.red, Color.red, Color.red, Color.red, Color.red };
+    Color[] btnPressedColors = { Color.red, Color.red, Color.red, Color.red, Color.red, Color.red };
+
     int defeatedEnemies=0;
     int tutorialStage = -1;
     bool[] keysPressed = { false, false, false, false, false, false };
@@ -19,7 +22,11 @@ public class TutorialScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int i=0; i<6; i++)
+        {
+            btnUnpressedColors[i] = unpressedCol;
+            btnPressedColors[i] = pressedCol;
+        }
     }
 
     // Update is called once per frame
@@ -47,6 +54,8 @@ public class TutorialScript : MonoBehaviour
         {
             tutorialStage++;
             shootInstructionLbl.SetActive(true);
+            pressedCol.a = 0.25f;
+            unpressedCol.a = 0.25f;
             //Play need press click to shoot now
         }
         if (defeatedEnemies == 1 && tutorialStage == 1)
@@ -63,43 +72,57 @@ public class TutorialScript : MonoBehaviour
         }
     }
 
+    void changeColOpacity(int colInd, float opacity)
+    {
+        Color newCol1 = pressedCol;
+        Color newCol2 = unpressedCol;
+        newCol1.a = opacity;
+        newCol2.a = opacity;
+        btnPressedColors[colInd] = newCol1;
+        btnUnpressedColors[colInd] = newCol2;
+    }
+
     public void PlayerMove(InputAction.CallbackContext ctx)
     {
         Vector2 inp = ctx.ReadValue<Vector2>();
         //Make keys still get darker when pressed even after faded
+        d.color = btnUnpressedColors[0];
+        a.color = btnUnpressedColors[1];
+        w.color = btnUnpressedColors[2];
+        s.color = btnUnpressedColors[3];
         if (inp.x > 0)
         {
+            d.color = btnPressedColors[0];
             if (!keysPressed[0])
             {
-                d.color = pressedCol;
-                StartCoroutine(fadeBtn(d));
+                StartCoroutine(fadeBtn(d,0));
                 keysPressed[0] = true;
             }
         }
         else if (inp.x < 0)
         {
+            a.color = btnPressedColors[1];
             if (!keysPressed[1])
             {
-                a.color = pressedCol;
-                StartCoroutine(fadeBtn(a));
+                StartCoroutine(fadeBtn(a,1));
                 keysPressed[1] = true;
             }
         }
         if (inp.y > 0)
         {
+            w.color = btnPressedColors[2];
             if (!keysPressed[2])
             {
-                w.color = pressedCol;
-                StartCoroutine(fadeBtn(w));
+                StartCoroutine(fadeBtn(w,2));
                 keysPressed[2] = true;
             }
         }
         else if (inp.y < 0)
         {
+            s.color = btnPressedColors[3];
             if (!keysPressed[3])
             {
-                s.color = pressedCol;
-                StartCoroutine(fadeBtn(s));
+                StartCoroutine(fadeBtn(s,3));
                 keysPressed[3] = true;
             }
         }
@@ -109,12 +132,16 @@ public class TutorialScript : MonoBehaviour
     {
         if (ctx.performed)
         {
+            ctrl.color = btnPressedColors[5];
             if (!keysPressed[5])
             {
-                ctrl.color = pressedCol;
-                StartCoroutine(fadeBtn(ctrl));
+                StartCoroutine(fadeBtn(ctrl,5));
                 keysPressed[5] = true;
             }
+        }
+        else
+        {
+            ctrl.color = btnUnpressedColors[5];
         }
     }
 
@@ -122,12 +149,17 @@ public class TutorialScript : MonoBehaviour
     {
         if (ctx.performed)
         {
+            space.color = btnPressedColors[4];
             if (!keysPressed[4])
             {
-                space.color = pressedCol;
-                StartCoroutine(fadeBtn(space));
+                //space.color = pressedCol;
+                StartCoroutine(fadeBtn(space,4));
                 keysPressed[4] = true;
             }
+        }
+        else
+        {
+            space.color = btnUnpressedColors[4];
         }
     }
 
@@ -170,13 +202,12 @@ public class TutorialScript : MonoBehaviour
         return true;
     }
 
-    IEnumerator fadeBtn(Image img)
+    IEnumerator fadeBtn(Image img,int _id)
     {
-        while (img.color.a > 0.25f)
+        while (img.color.a > 0.7f)
         {
-            Color col = Color.white;
-            col.a = img.color.a-0.01f;
-            img.color = col;
+            changeColOpacity(_id, img.color.a-0.01f);
+            img.color = btnUnpressedColors[_id];
             yield return null;
         }
     }
